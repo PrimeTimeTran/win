4704a172e59dc80397857dc90b2b0559103d08ce
Loi Tran <lvt15@my.fsu.edu>
2017-05-23 14:25:26 +0700

    completed some of the functionality Harley requested

--- todos-api/spec/rails_helper.rb
+++ todos-api/spec/rails_helper.rb
@@ -31,6 +31,7 @@ RSpec.configure do |config|
 
   # add `FactoryGirl` methods
   config.include FactoryGirl::Syntax::Methods
+  
 
   # start by truncating all the tables but then use the faster transaction strategy the rest of the time.
   config.before(:suite) do
