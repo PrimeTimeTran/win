ce15ab7fdff775623fb2c9c65625fe6b3162795c
Loi Tran <lvt15@my.fsu.edu>
2017-06-03 18:10:08 +0700

    completed todos api & testing project

--- hw1/config/routes.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.routes.draw do
-  # Define the path for menu is the home controller menu action
-  get 'menu' => 'home#menu'
-  get 'contact_us' => 'home#contact_us'
-  root 'home#index'
-end
