566826b7262edceceda2bd1de3c07f2106baa45b
Loi Tran <lvt15@my.fsu.edu>
2017-09-20 14:23:38 +0700

    Cleaned up the demo repo

--- react/manager/src/actions/types.js
+++
@@ -1,4 +0,0 @@
-// There will be many types of exports which is why it's NOT export default
-// Creates a variable to avoid many typoes that may happen
-export const EMAIL_CHANGED = 'email_changed';
-export const PASSWORD_CHANGED = 'password_changed';
